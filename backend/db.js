e996e674f62e03eac4774b2600c73758058ba7a3
NiteshKG <rachnakgupta81@gmail.com>
2023-08-07 16:38:24 +0530

    set up db schema in mongoose in node

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,20 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb+srv://niteshgupta8161:Nitesh@1234@cluster0.twzzcdv.mongodb.net/fooddeli?retryWrites=true&w=majority'
-const mongoDB =() =>{
-mongoose.connect(mongoURI, () =>{
+const mongoURI = 'mongodb+srv://niteshgupta8161:Nitesh@1234@cluster0.twzzcdv.mongodb.net/gofoodmern?retryWrites=true&w=majority'
+const mongoDB =async() =>{
+await mongoose.connect(mongoURI,{ useNewUrlParser: true }, async(err,result) =>{ 
+   if(err)console.log("---",err);
+   else{
     console.log("connected");
+    const fetched_data = await mongoose.Connection.db.collection("sample");
+    fetched_data.find({}).toArray(function(err,data){
+     if(err) console.log(err);
+     else{
+        
+     }
 
-});  
+    })
+   }
+});
 }
 
 module.exports = mongoDB;
